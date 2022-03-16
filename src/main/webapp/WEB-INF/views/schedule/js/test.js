038f16fd4927025e4a2dd89218e8b75e1769ff40
Nikita Babaylov <babaylov.nik@mail.ru>
2022-03-16 23:33:21 +0400

    added scrolling of months

--- src/main/webapp/WEB-INF/views/schedule/js/test.js
+++ src/main/webapp/WEB-INF/views/schedule/js/test.js
@@ -15,4 +15,22 @@ const date = new Date();
     "December",
   ];
 
-document.querySelector(".month h1").innerHTML = months[date.getMonth()];
+var month = date.getMonth();
+
+document.querySelector(".month h1").innerHTML = months[month];
+
+document.querySelector("#submitLeft").onclick = function() {
+month--;
+if(month < 0) {
+month = 11;
+}
+document.querySelector(".month h1").innerHTML = months[month];
+}
+
+document.querySelector("#submitRight").onclick = function() {
+month++;
+if(month > 11) {
+month = 0;
+}
+document.querySelector(".month h1").innerHTML = months[month];
+}
